644ba2c1daacc5af6dad4c436b2f7099e8b468df
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2025-07-29 19:16:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/2D Difference Array/2d-difference-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    vector<vector<int>> applyDiff2D(vector<vector<int>>& mat,
+                                    vector<vector<int>>& opr) {
+        // code here
+        
+         int n = mat.size(), m = mat[0].size();
+        vector<vector<int>> diff(n + 1, vector<int>(m + 1, 0));
+
+        for (auto& op : opr) {
+            int v = op[0], r1 = op[1], c1 = op[2], r2 = op[3], c2 = op[4];
+            diff[r1][c1] += v;
+            if (c2 + 1 < m) diff[r1][c2 + 1] -= v;
+            if (r2 + 1 < n) diff[r2 + 1][c1] -= v;
+            if (r2 + 1 < n && c2 + 1 < m) diff[r2 + 1][c2 + 1] += v;
+        }
+
+        for (int i = 0; i < n; i++)
+            for (int j = 1; j < m; j++)
+                diff[i][j] += diff[i][j - 1];
+
+        for (int j = 0; j < m; j++)
+            for (int i = 1; i < n; i++)
+                diff[i][j] += diff[i - 1][j];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                mat[i][j] += diff[i][j];
+
+        return mat;
+        
+    }
+};
