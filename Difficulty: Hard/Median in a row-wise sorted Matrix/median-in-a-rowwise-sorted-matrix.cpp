586f900037b03a2056becbf8fe34c740136fa04b
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2025-08-22 20:36:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,55 +1,11 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
     int median(vector<vector<int>> &mat) {
         // code here
-        vector<int> ans;
-        
-        for(int i = 0; i < mat.size(); i++){
-            for(int j = 0; j < mat[0].size(); j++){
-                ans.push_back(mat[i][j]);
-            }
-        }
-        
-        sort(ans.begin(), ans.end());
-        int n = mat.size() * mat[0].size();
-        
-        return ans[n/2];
+          vector<int>val;
+        for(int i=0;i<mat.size();i++)
+            for(int j=0;j<mat[0].size();j++) val.push_back(mat[i][j]);
+        sort(val.begin(),val.end());
+        return val[(mat.size()*mat[0].size())/2];
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int r, c;
-        cin >> r >> c;
-        vector<vector<int>> matrix(r, vector<int>(c));
-        for (int i = 0; i < r; ++i)
-            for (int j = 0; j < c; ++j)
-                cin >> matrix[i][j];
-        Solution obj;
-        int ans = -1;
-        ans = obj.median(matrix);
-        cout << ans << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
