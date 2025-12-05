af150c39e1238e3384a06ef148eed48892b1916d
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2025-12-05 12:36:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int factorial(int n) {
+        // code here
+        int fact=1;
+        for(int i=n; i>=1;i--){
+            fact = fact*i;
+        }
+        return fact;
+    }
+};
