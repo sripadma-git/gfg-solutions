9ce559a05e6a0505e348cc6b5239e10aeab9b03a
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2026-02-11 08:18:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Equalize the Towers/equalize-the-towers.cpp
+++ Difficulty: Medium/Equalize the Towers/equalize-the-towers.cpp
@@ -1,28 +1,43 @@
 class Solution {
   public:
-        // code here
-         long long getCost(vector<int>& heights, vector<int>& cost, int target) {
-        long long total = 0;
-        for (int i = 0; i < heights.size(); ++i)
-            total += 1LL * abs(heights[i] - target) * cost[i];
-        return total;
-    }
-
+  
     int minCost(vector<int>& heights, vector<int>& cost) {
-        int low = *min_element(heights.begin(), heights.end());
-        int high = *max_element(heights.begin(), heights.end());
-        long long ans = LLONG_MAX;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            long long cost1 = getCost(heights, cost, mid);
-            long long cost2 = getCost(heights, cost, mid + 1);
-            ans = min(cost1, cost2);
-
-            if (cost1 < cost2) high = mid - 1;
-            else low = mid + 1;
+        // code here
+        int l = *min_element(heights.begin(), heights.end());
+        int r = *max_element(heights.begin(), heights.end());
+        int min_cost = INT_MAX;
+        bool flag = true;
+        
+        function<int(int)> helper = [&](int mid)->int
+        {
+            int c = 0;
+            for(int i = 0;i<heights.size();i++)
+            {
+                c = c + abs(mid - heights[i]) * cost[i];
+            }
+            return c;
+        };
+        while(l<=r && flag)
+        {
+            int mid = (l+r)/2;
+            int c = helper(mid);
+            int p = helper(mid-1);
+            int n = helper(mid+1);
+            if(c<=p && c<=n)
+            {
+                min_cost = c;
+                flag = false;
+                break;
+            }
+            else if(c>p && c<=n)
+            {
+                r = mid -1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
         }
-
-        return (int)ans;
+        return min_cost;
     }
 };
