9b89726a7dbda9ba726a3c9a60a2bc9614844871
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2026-02-19 08:01:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing Element in Range/missing-element-in-range.cpp
+++ Difficulty: Medium/Missing Element in Range/missing-element-in-range.cpp
@@ -1,15 +1,24 @@
 class Solution {
   public:
-    vector<int> missinRange(vector<int>& arr, int low, int high) {
-        unordered_set<int> elements(arr.begin(), arr.end());
-        vector<int> missing;
-
-        for (int num = low; num <= high; ++num) {
-            if (elements.find(num) == elements.end()) {
-                missing.push_back(num);
-            }
-        }
-
-        return missing;
+    virtual vector<int> missinRange(vector<int>& arr, int low, int high) {
+      sort(arr.begin(), arr.end());
+      
+      int si=lower_bound(arr.begin(), arr.end(), low)-arr.begin();
+      int ei=lower_bound(arr.begin(), arr.end(), high)-arr.begin();
+      
+      int n=arr.size();
+      unordered_map<int, bool> visited; 
+      while(si<=ei){
+        if(si==n) break;
+        visited[arr[si]]=true;
+        si++;
+      }
+      
+      vector<int> ans;
+      for(int i=low; i<=high; i++){
+        if(!visited[i]) ans.push_back(i);  
+      }
+      
+      return ans;
     }
 };
