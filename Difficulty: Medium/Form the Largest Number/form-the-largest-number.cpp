7cc90cdb3abb7a614efc07ce82c95676362deb04
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2026-02-20 10:17:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
@@ -1,26 +1,25 @@
 class Solution {
   public:
-static bool help(string &a , string &b){
-        return a + b > b + a;
-    }
     string findLargest(vector<int> &arr) {
-            vector<string> brr;
-            
-            for(auto &i : arr){
-                brr.push_back(to_string(i));
-            }
-            
-            sort(brr.begin() , brr.end() , help);
-            
-            string ans = "";
-            
-            for(auto &i : brr){
-                ans += i;
-            }
-            
-            if(ans[0] == '0') return "0";
-            
-            return ans;
-        
+        // code here
+        vector<string> sample;
+        for (int num : arr) {
+            sample.push_back(to_string(num));
+        }
+
+        sort(sample.begin(), sample.end(), [](const string& a, const string& b) {
+            return (a + b) > (b + a);
+        });
+
+        if (sample[0] == "0") {
+            return "0";
+        }
+
+        string ans = "";
+        for (const string& s : sample) {
+            ans += s;
+        }
+
+        return ans;
     }
 };
