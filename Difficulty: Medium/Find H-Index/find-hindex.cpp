5346958f5246e04fcb3be8fa1134cc5d05da359e
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2026-02-21 08:49:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -2,12 +2,14 @@ class Solution {
   public:
     int hIndex(vector<int>& citations) {
         // code here
-         int n=citations.size();
+        int n=citations.size();
         int ans=0;
-        sort(citations.begin(), citations.end()); // O(NlogN)
-        for(int i=0;i<n;i++){
-            if(citations[i]>=n-i) return n-i;
+        sort(citations.begin(),citations.end(),greater<>());
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]>=i+1){
+                ans= i+1;
+            }
         }
-        return 0;
+        return ans;
     }
 };
