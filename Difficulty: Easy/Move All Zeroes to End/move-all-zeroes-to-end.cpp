f991ea33cd5896a85340904839be255d3677f4be
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2026-03-01 09:21:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int cnt = 0;
+        for(int i = 0;i<arr.size();i++){
+            if(arr[i] == 0)cnt++;
+            if(arr[i] and cnt){
+                swap(arr[i-cnt], arr[i]);
+            }
+        }
+    }
+};
